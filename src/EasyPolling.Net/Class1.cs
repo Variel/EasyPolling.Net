b93e014f0bba9fb4b6de6f84c6409c835f0dddb9
이예찬 <blackmagic2@naver.com>
2016-10-31 21:52:03 +0900

    초기 커밋.

---
+++ src/EasyPolling.Net/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyPolling.Net
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
